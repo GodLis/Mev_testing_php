f7b1a4cf12a3aa3ebe389c01ba61587810709d92
GodLis <olechka.brajko@gmail.com>
2016-07-10 23:59:42 +0300

    update psr

--- task19/src/GodLis/Watermark.php
+++ task19/src/GodLis/Watermark.php
@@ -6,8 +6,16 @@ namespace GodLis\Task19;
  * Class Watermark
  * @package GodLis
  */
+/**
+ * Class Watermark
+ * @package GodLis\Task19
+ */
 class Watermark
 {
+    /**
+     * @param $sOrigImg
+     * @param $sWmImg
+     */
     public function watermark($sOrigImg, $sWmImg)
     {
         if (! extension_loaded('gd')) { // Проверяем установку библиотеки GD
@@ -46,7 +54,16 @@ class Watermark
 
             if (is_array($aWmImgInfo) && count($aWmImgInfo)) {
                 $rWmImage = imagecreatefrompng($sWmImg);
-                imagecopy($rImage, $rWmImage, $iSrcWidth-$aWmImgInfo[0], $iFrameSize, 0, 0, $aWmImgInfo[0], $aWmImgInfo[1]);
+                imagecopy(
+                    $rImage,
+                    $rWmImage,
+                    $iSrcWidth-$aWmImgInfo[0],
+                    $iFrameSize,
+                    0,
+                    0,
+                    $aWmImgInfo[0],
+                    $aWmImgInfo[1]
+                );
             }
 
             imagepng($rImage, "imageWithWatermark.png");
