04f9eccd7b56e302a43dc12fedd8cd4e65c1bc32
GodLis <olechka.brajko@gmail.com>
2016-07-16 16:51:49 +0300

    add PHPDoc, update PSR2, update OOP

--- task4/index.php
+++ task4/index.php
@@ -1,30 +1,55 @@
 <?php
-
+/**
+ * Index File Doc Comment
+ *
+ * This file is a part of a task3
+ *
+ * PHP version 7
+ *
+ * @category Testing
+ * @package  GodLis\Task4
+ * @author   Olechka Brajko <olechkabrajko@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/GodLis/Mev_testing_php
+ */
 require __DIR__ .'/vendor/autoload.php';
 
-$k = mt_rand(20, 30);
-$array1[] = [];
-$array2[] = [];
+//$k = mt_rand(20, 30);
+//$array1[] = [];
+//$array2[] = [];
+//
+//for ($i=0; $i<$k; $i++) {
+//    $array1[$i] = random_int(1, 30);
+//    $array2[$i] = random_int(1, 30);
+//};
+//
+//echo "Массив 1\n";
+//for ($i=0; $i<$k; $i++) {
+//    echo ($array1[$i] ." ");
+//};
+//
+//echo "\nМассив 2\n";
+//for ($i=0; $i<$k; $i++) {
+//    echo ($array2[$i] ." ");
+//};
 
-for ($i=0; $i<$k; $i++) {
-    $array1[$i] = random_int(1, 30);
-    $array2[$i] = random_int(1, 30);
-};
-
-echo "Массив 1\n";
-for ($i=0; $i<$k; $i++) {
-    echo ($array1[$i] ." ");
-};
-
-echo "\nМассив 2\n";
-for ($i=0; $i<$k; $i++) {
-    echo ($array2[$i] ." ");
-};
+$file = 'config/config.ini';
+$config = parse_ini_file($file);
+$arrToQuickSort = $arr = $config['arr'];
+echo "Array 1:\n";
+print_r($config['array1']);
+echo "Array 2:\n";
+print_r($config['array2']);
 
 echo "\nНахождение общих елементов функцией 'array_intersect':\n";
-print_r($result = array_intersect($array1, $array2));
+
+if (!empty($config['array1'] && $config['array2'])) {
+    print_r($result = array_intersect($config['array1'], $config['array2']));
+}
 
 echo "Нахождение общих елементов функцией пользователя:\n";
-$tmp = new \GodLis\Task4\UserFunction();
-print_r($tmp->userFunction($array1, $array2));
+if (!empty($config['array1'] && $config['array2'])) {
+    $tmp = new \GodLis\Task4\UserFunction($config);
+    print_r($tmp->userFunction());
+}
 
--- task4/src/GodLis/UserFunction.php
+++ task4/src/GodLis/UserFunction.php
@@ -1,45 +1,91 @@
 <?php
-
+/**
+ * UserFunction File Doc Comment
+ *
+ * This file is a part of a task3
+ *
+ * PHP version 7
+ *
+ * @category Testing
+ * @package  GodLis\Task4
+ * @author   Olechka Brajko <olechkabrajko@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/GodLis/Mev_testing_php
+ */
 namespace GodLis\Task4;
 
 /**
  * Class UserFunction
- * @package GodLis
+ *
+ * @category Testing
+ * @package  GodLis\Task3
+ * @author   Olechka Brajko <olechkabrajko@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/GodLis/Mev_testing_php
  */
 class UserFunction
 {
     /**
-     * @param $array1
-     * @param $array2
+     * UserFunction variable
+     *
+     * @var
+     */
+    protected $array1;
+
+    /**
+     * UserFunction variable
+     *
+     * @var
+     */
+    protected $array2;
+
+    /**
+     * UserFunction constructor.
+     *
+     * @param array $config Configs that are transferred to use
+     */
+    public function __construct($config)
+    {
+        $this->array1 = $config['array1'];
+        $this->array2 = $config['array2'];
+    }
+
+    /**
+     * UserFunction destructor.
+     */
+    public function __destruct()
+    {
+        $this->array1;
+        $this->array2;
+    }
+
+    /**
+     * UserFunction Function
+     *
      * @return array
      */
-    public function userFunction($array1, $array2)
+    public function userFunction()
     {
         $arr[] = [];
-        $k = 0;
-        if (count($array1)>=count($array2)) {
-            for ($i = 0; $i < count($array1); $i++) {
-                for ($j = 0; $j < count($array2); $j++) {
-                    if ($array1[$i] == $array2[$j]) {
-                        $arr[$k] = $array1[$i];
-                        $k++;
+        if (count($this->array1) >= count($this->array2)) {
+            for ($i = 0; $i < count($this->array1); $i++) {
+                for ($j = 0; $j < count($this->array2); $j++) {
+                    if ($this->array1[$i] == $this->array2[$j]) {
+                        $arr[$i] = $this->array1[$i];
                         break;
                     }
                 }
             }
-            return $arr;
-        } elseif (count($array2)>count($array1)) {
-            for ($i = 0; $i < count($array2); $i++) {
-                for ($j = 0; $j < count($array1); $j++) {
-                    if ($array2[$i] == $array1[$j]) {
-                        $arr[$k] = $array2[$i];
-                        $k++;
+        } elseif (count($this->array2) > count($this->array1)) {
+            for ($i = 0; $i < count($this->array2); $i++) {
+                for ($j = 0; $j < count($this->array1); $j++) {
+                    if ($this->array2[$i] == $this->array1[$j]) {
+                        $arr[$i] = $this->array2[$i];
                         break;
                     }
                 }
             }
-            return $arr;
         }
-        return false;
+        return $arr;
     }
 }
